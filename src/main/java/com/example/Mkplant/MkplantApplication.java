e73d469f1c7111688f6c355586704956b19cd673
GaoXiang <2643941443@qq.com>
2022-11-22 20:29:40 +0800

    first commit

---
+++ src/main/java/com/example/Mkplant/MkplantApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mkplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkplantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkplantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/Mkplant/MkplantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mkplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkplantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
